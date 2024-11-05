4a047ea1aea1b5aebdb548caefb95812c21c3d03
Arthur_msm <arthur_m_martins@estudante.sesisenai.org.br>
2024-11-05 12:02:04 -0300

    recomeco

---
+++ ord.js
@@ -0,0 +1,101 @@
+let array = []
+let res = document.getElementById("res")
+let resto = 0
+
+function add(){
+    let num = Number(document.getElementById("num").value)
+    array.push(num)
+    res.innerHTML = array
+}
+function escolha(){
+    
+}
+function ordena(){
+    var radios = document.getElementsByName("sort");
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            console.log("Escolheu: " + radios[i].value);
+            if (radios[i].value == "Bubble"){
+                let reverse = document.getElementById("reverse");
+                if (reverse.checked == true){
+                    for(i=0;i<array.length;i++){
+                        for(j=1;j<array.length;j++){
+                            if(array[j - 1] < array[j]){
+                                resto = array[j]
+                                array[j] = array[j-1]
+                                array[j-1] = resto
+                            }
+                        }
+                    }
+                } else {
+                    for(i=0;i<array.length;i++){
+                        for(j=1;j<array.length;j++){
+                            if(array[j - 1] > array[j]){
+                                resto = array[j]
+                                array[j] = array[j-1]
+                                array[j-1] = resto
+                            }
+                        }
+                    }
+                }
+            } else if (radios[i].value == "Insertion"){
+                let reverse = document.getElementById("reverse");
+                if (reverse.checked == true){
+                    for(i=1;i<array.length;i++){
+                        let j = i - 1
+                        valor_atual = array[i]
+                        while(j>=0 && valor_atual > array[j]){
+                        array[j+1]=array[j]
+                        j--
+                        }
+                        array[j+1]=valor_atual
+                    }
+                } else {
+                    for(i=1;i<array.length;i++){
+                        let j = i - 1
+                        valor_atual = array[i]
+                        while(j>=0 && valor_atual < array[j]){
+                        array[j+1]=array[j]
+                        j--
+                        }
+                        array[j+1]=valor_atual
+                    }
+                }
+            } else {
+                let reverse = document.getElementById("reverse");
+                if (reverse.checked == true){
+                    for(i=0;i<array.length;i++){
+                        menor = i
+                        for(j=i+1;j<array.length;j++){
+                        if(array[j]>array[menor]){
+                        menor = j
+                        }
+                        }
+                        if(i != menor){
+                        valor_atual = array[i]
+                        array[i]=array[menor]
+                        array[menor]=valor_atual
+                        }
+                        }
+                } else {
+                    for(i=0;i<array.length;i++){
+                        menor = i
+                        for(j=i+1;j<array.length;j++){
+                        if(array[j]<array[menor]){
+                        menor = j
+                        }
+                        }
+                        if(i != menor){
+                        valor_atual = array[i]
+                        array[i]=array[menor]
+                        array[menor]=valor_atual
+                        }
+                    }
+                }
+            }
+                        
+            res.innerHTML = array
+        }
+    }
+    
+}
